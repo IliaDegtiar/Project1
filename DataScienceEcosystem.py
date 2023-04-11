a778a0f0f890335a17176ea0d64d5ce2bf175664
IliaDegtiar <130287551+IliaDegtiar@users.noreply.github.com>
2023-04-11 17:21:25 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# *In this notebook, Data Science Tools and Ecosystem are summarized.*
+
+# # Objectives:
+# - Apache MLlib. 
+# - Deeplearning4 Java. 
+# - Java-ML. 
+# - JavaScript. 
+# - Julia. 
+# - Python. 
+# - R. 
+# - Scala. 
+# - SQL. 
+# - Weka.
+
+# # Some of the popular languages that Data Scientists use are:
+# ##### 1.Apache MLlib 
+# ##### 2.Deeplearning4.
+# ##### 3.Java.
+# ##### 4.Java-ML.
+# ##### 5.JavaScript.
+# ##### 6.Julia.
+# ##### 7.Python.
+# ##### 8.R.
+# ##### 9.Scala.
+# ##### 10.SQL.
+# ##### 11.Weka.
+# 
+
+# # Some of the commonly used libraries used by Data Scientists include:
+# 
+
+# #### 1.Lattice.
+# #### 2.NumPy 
+# #### 3. Pandas. 
+# #### 4.Tensorflow.
+# #### 5. Jupyter.
+# #### 6. ggplot.
+
+# # Data Science Tools
+# #### Hadoop, Jupyter Lab, RStudio
+# #### Matplotlib,Plotly
+# #### SaaS,Watson Studio
+# 
+# 
+
+# # Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[14]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[15]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# I.K. Degtiarev
+# 
